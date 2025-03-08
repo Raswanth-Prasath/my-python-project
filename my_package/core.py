b7dd25bb9f10aaa5a57a66fd07d6fa7f5a122033
Raswanth Prasath <raswanthprasath27@gmail.com>
2025-03-08 01:29:36 -0700

    Add core module

---
+++ my_package/core.py
@@ -0,0 +1,18 @@
+"""Core functionality for my_package."""
+
+def hello_world():
+    """Return a hello world message."""
+    return "Hello, World!"
+
+
+def add(a, b):
+    """Add two numbers.
+    
+    Args:
+        a: First number
+        b: Second number
+        
+    Returns:
+        Sum of a and b
+    """
+    return a + b
